e489239f57713de22cb68903a573d5603c9ff15f
name <dhimanamandeep09@gmail.com>
2026-03-30 23:26:49 +0530

    add token and auth in token add token in api

--- src/Routes/auth.routes.js
+++ src/Routes/auth.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-// import authController from '../controllers/auth.controller.js'
+import * as authController from '../controllers/auth.controller.js'
 
 const authRouter = Router()
 
 // post route "/api/auth/register"
 
-authRouter.post("/register")
+authRouter.post("/register", authController.register)
+
+// get route "/api/auth/get-me"
+
+authRouter.get("/get-me",authController.getMe)
 
 
 export default authRouter
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
-import UserModel from "../models/user.models";
+import UserModel from "../models/user.models.js";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import config from "../config/config.js";
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
 
@@ -12,19 +14,61 @@ const register = async (req, res) => {
             ]
         })
         if (isAlreadyRegisterd) {
-            res.status(409).json({
+            return res.status(409).json({
                 message: "Username or email already existed.."
             })
         }
 
-        const hashedPassword = bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)
 
         const user = await UserModel.create({
             username,
             email,
             password: hashedPassword
         })
-    } catch (error) {
 
+        const token = jwt.sign({
+            id: user._id,
+        }, config.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+
+        res.status(201).json({
+            message: "User register successfully..",
+            user: {
+                username: username,
+                email: email
+            },
+            token // this is a token genewrated in 
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
     }
+}
+
+export const getMe = async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            meassge: "token not found"
+        })
+    }
+
+    const decoded = jwt.verify(token, config.JWT_SECRET)
+
+    // console.table(decoded)
+
+    const user = await UserModel.findById(decoded.id)
+
+    res.status(200).json({
+        meassge: "User fetched successfully",
+        user: {
+            username: user.username,
+            email: user.email
+        }
+    })
 }
